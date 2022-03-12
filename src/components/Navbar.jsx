086dbc3c52bf0666a188568a1e864bd9bce631a0
siddharthmishra95 <siiid.mishra@gmail.com>
2022-03-12 09:33:35 +0530

    Created Slider Navbar and Home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { Search } from "@material-ui/icons";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import { Badge } from "@material-ui/core";
 import React from "react";
 import styled from "styled-components";
 
@@ -9,12 +10,22 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            input
-            <Search />
+            <Input />
+            <Search style={{ color: "gray", fontSize: 16 }} />
           </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Center>
+          <Logo>SIDDHARTH...</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="secondary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
@@ -27,6 +38,7 @@ const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 const Left = styled.div`
   display: flex;
@@ -41,6 +53,9 @@ const Language = styled.span`
 
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 `;
 const Center = styled.div`
   flex: 1;
@@ -50,6 +65,23 @@ const SearchContainer = styled.div`
   border: 1px solid lightgray;
   display: flex;
   align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  text-align: center;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
 `;
 
 export default Navbar;
